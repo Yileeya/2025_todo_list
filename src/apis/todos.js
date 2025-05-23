a27de80e5592745dc23eb26ee8b66b70417b940d
Yilee <yileefrontend@gmail.com>
2025-05-23 15:40:33 +0800

    api 設定

--- src/apis/todos.js
+++ src/apis/todos.js
@@ -1,5 +1,34 @@
 import http from '@/utils/http.js';
 
+const url = {
+    state: '/state',
+    todo: '/TodoList'
+};
+
+export function getState() {
+    return http.get(url.state);
+}
+
 export function getTodos() {
-    return http.get('/todos');
+    return http.get(url.todo);
+}
+
+export function getTodo(id) {
+    return http.get(`${url.todo}/${id}`);
+}
+
+export function postTodo(form) {
+    return http.post(url.todo, {
+        ...form
+    });
+}
+
+export function putTodo(id, form) {
+    return http.put(`${url.todo}/${id}`, {
+        ...form
+    });
+}
+
+export function deleteTodo(id) {
+    return http.delete(`${url.todo}/${id}`);
 }
