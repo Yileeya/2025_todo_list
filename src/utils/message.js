af3d91674c773b01225e7e22a82f285cc38c9bab
Yilee <yileefrontend@gmail.com>
2025-05-23 16:39:58 +0800

    message toast ui z-index 調整

--- src/utils/message.js
+++ src/utils/message.js
@@ -4,6 +4,7 @@ import 'element-plus/theme-chalk/src/message.scss';
 export function showMessage(type = 'success', message = '操作成功') {
     ElMessage({
         type: type,
-        message: message
+        message: message,
+        customClass: 'toast-message-index'
     });
 }
